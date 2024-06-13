f7995d6d45958603846485ad5480be7f3c984a1b
Mitchell-Sella <77133406+Mitchell-Sella@users.noreply.github.com>
2024-06-13 04:06:58 +0000

    Added get_user_id function

--- Hello.py
+++ Hello.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import requests
+
+def get_user_id(username):
+    base_url = 'https://api.sleeper.app'
+    endpoint = f'/v1/user/{username}'
+    response = requests.get(base_url + endpoint)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
 
 def run():
     st.set_page_config(
         page_title="Home",
         page_icon="https://sleeper.com/favicon.ico",
     )
+
+
 
 if __name__ == "__main__":
     run()
